995d8559ec06cb6c930301332982c765de6d86f1
VashishthSingh <42479077+VashishthSingh@users.noreply.github.com>
2022-01-11 14:30:17 +0530

    Update JdbcDemo.java

--- JavaJdbcDemo/src/com/jdbcjava/JdbcDemo.java
+++ JavaJdbcDemo/src/com/jdbcjava/JdbcDemo.java
@@ -8,6 +8,7 @@ import java.sql.PreparedStatement;
 
 public class JdbcDemo {
 	public static void main(String[] args) {
+		// Muni Singh
 		Connection con=null;
 		Statement stmt=null;
 		ResultSet rs=null;
